ea6d69b488a24274ff64435ddd954683f77798ba
RA9T0R <Phongphatbangkha@gmail.com>
2025-01-04 19:44:51 +0700

    Setup Project
    Create folder for frontend,backend,admin,document,sub folder and install some tools

---
+++ Admin/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      rrr
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      xxx
+    </div>
+  )
+}
+
+export default App
