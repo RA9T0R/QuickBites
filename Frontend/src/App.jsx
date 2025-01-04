42c0ec6cd5b0556e3ab9760a15ecd69ec12a6538
RA9T0R <Phongphatbangkha@gmail.com>
2025-01-04 20:07:45 +0700

    Commit 1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div>
-      xxx
+      <ToastContainer position='top-right' autoClose={2000}/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+
+      </Routes>
+      <Footer/>
     </div>
   )
 }
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      xxx
+    </div>
+  )
+}
+
+export default Navbar
