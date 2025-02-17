d543615e4e77abdcdd5534c210c5de714e5a8935
6604062630561 <s6604062630561@email.kmutnb.ac.th>
2025-02-17 21:14:45 +0700

    Update customerModel.js

--- Backend/models/customerModel.js
+++ Backend/models/customerModel.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const customerSchema = new mongoose.Schema({
     name: { type: String, required: true},
     table: {type : String, required: true},
     QRcode: {type : Object, default:{}}
 },{minimize:false})
 
-const userModel = mongoose.models.user || mongoose.model('user',userSchema);
+const customerModel = mongoose.models.user || mongoose.model('customer',customerSchema);
 
-export default userModel;
+export default customerModel;
