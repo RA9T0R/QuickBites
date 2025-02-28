ffe9d64c0b43e10638649c1c7bb975c1de1183e4
RA9T0R <Phongphatbangkha@gmail.com>
2025-02-28 16:54:00 +0700

    Revert "Update Login.jsx"
    This reverts commit 929ce4444209044a0a7ae9a48602a113a9ad1d4d.

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -1,38 +1,65 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState } from 'react';
+import axios from 'axios';
 import { backendURL } from '../App';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
-const Login = ({setToken}) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const isValid = async(e) => {
-        try {
-            e.preventDefault();
-            const response = await axios.post(backendURL + "/api/employee/admin", {email, password});
-            if (response.data.success) {
-                setToken(response.data.token)
-            }
-            else {
-                toast.error(response.data.message)
-            }
-        } catch (error) {
-            console.log(error)
-            toast.error(error.message)
-        }
+const Login = ({ setToken, setRole }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const isValid = async (e) => {
+    e.preventDefault();
+    const endpoint = "/api/employee/login";
+    try {
+      const response = await axios.post(backendURL + endpoint, { email, password });
+
+      if (response.data.success) {
+        navigate("/");
+        setToken(response.data.token);
+        setRole(response.data.role);
+        toast.success(`Logged in as ${response.data.role === "admin" ? "Admin" : "Employee"}`);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-        <h2 className="text-2xl font-bold text-gray-900 mb-4">Login</h2>
+        <h2 className="text-2xl font-bold text-gray-900 mb-4">
+          Login
+        </h2>
         <form className="flex flex-col" onSubmit={isValid}>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Email address" />
-          <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150" placeholder="Password" />
-          <button type="submit" className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150">Login</button>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            placeholder="Email address"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            className="bg-gray-100 text-gray-900 border-0 rounded-md p-2 mb-4 focus:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-500"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 transition ease-in-out duration-150"
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
