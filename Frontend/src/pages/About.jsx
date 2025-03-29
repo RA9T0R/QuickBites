34a4c36f9a725577c684c3c130f1c42592070208
6604062630251 <s6604062630251@email.kmutnb.ac.th>
2025-03-30 00:22:47 +0700

    Frontend Adjustment V.1
    • Replace Template Text:
            - Thank You Page
            - About us Page

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
@@ -11,11 +11,14 @@ const About = () => {
       <div className='my-10 flex flex-col md:flex-row gap-16 '>
         <img className='w-full md:max-w-[450px]' src={assets.Home} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-Text'>
-          <p>Forever was born out of a passion for innovation and a desire to make a positive impact on the world. We believe that technology has the power to transform our lives and make a difference in the world around us.</p>
-          <p>Since our incaption, we've worked tirelessly to curate a collection of cutting-edge products that empower individuals and businesses to thrive in the digital age. From cutting-edge gadgets to innovative solutions, we've got you covered
+          <p>
+            QuickBites was founded with a simple goal — to make dining easier, faster, and more enjoyable. In a world where time is precious, we believe technology can enhance your restaurant experience without taking away the human touch.
+          </p>
+          <p>
+            From effortless menu browsing to real-time order tracking, we’ve designed QuickBites to bring convenience to both diners and restaurant staff. Whether you're a hungry guest or a hardworking team member, we’ve got your back with smart, seamless tools that just work.
           </p>
           <b className='text-Text'>Our Mission</b>
-          <p>At Forever, our mission is to empower individuals and businesses to navigate the ever-evolving digital landscape with confidence and ease.</p>
+          <p>At QuickBites, our mission is to revolutionize the way people dine by blending technology with hospitality — making every meal not just a bite, but an experience.</p>
         </div>
       </div>
       <div className='w-full font-bold sm:font-semibold text-2xl sm:text-5xl text-start sm:text-center mb-3 text-Text'>
